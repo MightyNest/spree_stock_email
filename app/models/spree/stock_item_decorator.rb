631d121e4d1118bce2f7c50d260581a5233a0a52
Jonathan Kelley <jonathan.kelley@gmail.com>
2017-09-15 14:21:46 -0500

    Add threshold for sending stock notifications

--- app/models/spree/stock_item_decorator.rb
+++ app/models/spree/stock_item_decorator.rb
@@ -2,6 +2,8 @@ Spree::StockItem.class_eval do
   after_save :send_stock_emails
 
   def send_stock_emails
-    Spree::StockEmail.notify(variant) if variant.in_stock?
+    if variant.in_stock? && variant.total_on_hand >= Spree::StockEmailConfig::Config.notify_threshold
+      Spree::StockEmail.notify(variant)
+    end
   end
 end
